16368ffb1ba318d80041cba97534fff681887bbd
nandadegcarvalho <nandadeg@icloud.com>
2022-11-21 09:37:30 -0300

    Update desafio1novoform.js

--- desafio1novoform.js
+++ desafio1novoform.js
@@ -29,4 +29,39 @@ botaoAdicionar.addEventListener("click", function(event) {
     var tabela = document.querySelector("#tabela-clientes");
 
     tabela.appendChild(clienteTr);
+
+    somarAsRendas()
+
+    form.reset()
 });
+
+    function ajustaRenda(renda){
+    let rendaConvertida = parseFloat(renda).toLocaleString('pt-br',{style: 'decimal',minimumFractionDigits:2})
+    return rendaConvertida
+}
+
+    function somarAsRendas(){
+        var clientes = document.querySellectorAll(".cliente")
+        var somaFloat = 0
+        console.log('clientes:',clientes)
+        console.log('somaFloat;',somaFloat)
+
+        for (var i = 0; i < clientes.length; i++) {
+            var cliente = clientes[i];
+            var tdRenda = cliente.querySelector(".info-renda");
+            var rendaString = tdRenda.textContent
+            var rendaFloat = parseFloat(rendaString.toString().replace(/\./g,'').replace(',','.'))
+            somaFloat = somaFloat + somaFloat
+            console.log('cliente:', cliente)
+            console.log('tdRenda:',tdRenda)
+            console.log('rendaString:',rendaString)
+            console.log('rendaFloat',rendaFloat)
+
+        }
+
+        var somatorioRendas = document.querySelector("#totalizadorRendas")
+        var somaString = somaFloat.toLocaleString('pt-br',{style: 'decimal',minimumFractionDigits:2})
+        somatorioRendas.innerHTML = somaString
+    }
+    
+
