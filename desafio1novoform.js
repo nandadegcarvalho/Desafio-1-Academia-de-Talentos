f414880d7a7f8b4550b9aa0c26a26b4053f229e0
nandadegcarvalho <nandadeg@icloud.com>
2022-11-22 07:11:34 -0300

    tentando somar renda

--- desafio1novoform.js
+++ desafio1novoform.js
@@ -29,8 +29,7 @@ botaoAdicionar.addEventListener("click", function(event) {
     var tabela = document.querySelector("#tabela-clientes");
 
     tabela.appendChild(clienteTr);
-
-    somarAsRendas()
+   
 
     form.reset()
 });
@@ -40,28 +39,12 @@ botaoAdicionar.addEventListener("click", function(event) {
     return rendaConvertida
 }
 
-    function somarAsRendas(){
-        var clientes = document.querySellectorAll(".cliente")
-        var somaFloat = 0
-        console.log('clientes:',clientes)
-        console.log('somaFloat;',somaFloat)
+    var arr = [rendaTd]
+    var soma = arr.reduce(funcion(soma,i) {
+        return soma + i;
+    });
 
-        for (var i = 0; i < clientes.length; i++) {
-            var cliente = clientes[i];
-            var tdRenda = cliente.querySelector(".info-renda");
-            var rendaString = tdRenda.textContent
-            var rendaFloat = parseFloat(rendaString.toString().replace(/\./g,'').replace(',','.'))
-            somaFloat = somaFloat + somaFloat
-            console.log('cliente:', cliente)
-            console.log('tdRenda:',tdRenda)
-            console.log('rendaString:',rendaString)
-            console.log('rendaFloat',rendaFloat)
 
-        }
 
-        var somatorioRendas = document.querySelector("#totalizadorRendas")
-        var somaString = somaFloat.toLocaleString('pt-br',{style: 'decimal',minimumFractionDigits:2})
-        somatorioRendas.innerHTML = somaString
-    }
     
 
